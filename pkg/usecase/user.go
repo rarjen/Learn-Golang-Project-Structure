d384011c7737f2f1ac295ac1cd142da22e4f9f41
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-05 17:18:27 +0700

    update user.go

--- pkg/usecase/user.go
+++ pkg/usecase/user.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"template-ulamm-backend-go/pkg/errs"
 	"template-ulamm-backend-go/pkg/model/entity"
@@ -13,12 +14,15 @@ import (
 	"template-ulamm-backend-go/pkg/repository"
 
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 type UserUsecase interface {
-	FindAllUser(ctx context.Context) ([]entity.User, error)
-	FindUserById(ctx context.Context, id string) (entity.User, error)
+	// FindAllUser(ctx context.Context) ([]entity.User, error)
+	FindUserByEmployeeId(ctx context.Context, id string) (*entity.User, error)
 	SaveUser(ctx context.Context, req request.CreateUserRequest) (*response.CreatedUserResponse, error)
+	UpdateByEmployeeIdUseCase(ctx context.Context, req request.UpdateUserRequest, req_id request.GetIdUserRequest) (*response.UpdatedUserResponse, error)
+	DeleteByEmployeeIdUseCase(ctx context.Context, req_id request.GetIdUserRequest) (int, error)
 }
 
 type userUsecase struct {
@@ -29,21 +33,78 @@ func NewUserUsecase(userRepository repository.UserRepository) *userUsecase {
 	return &userUsecase{userRepository}
 }
 
-func (cu *userUsecase) FindAllUser(ctx context.Context) ([]entity.User, error) {
-	campaigns, err := cu.repository.FindAll(ctx)
+func (cu *userUsecase) UpdateByEmployeeIdUseCase(ctx context.Context, req request.UpdateUserRequest, req_id request.GetIdUserRequest) (*response.UpdatedUserResponse, error) {
+	userExist, err := cu.repository.FindByEmployeeId(ctx, req_id.IDEmployee)
 	if err != nil {
-		return campaigns, err
+		return nil, err
 	}
 
-	return campaigns, nil
+	userExist.IDEmployee = req.IDEmployee
+	userExist.Username = req.Username
+	userExist.Name = req.Name
+	userExist.IsActive = req.IsActive
+	userExist.CreatedBy = req.CreatedBy
+	userExist.CreatedTime = time.Now()
+	userExist.ModifiedBy = req.ModifiedBy
+	userExist.ModifiedTime = time.Now()
+
+	err = cu.repository.UpdateByEmployeeIdRepo(ctx, userExist)
+	if err != nil {
+		utils.GetLogger().Error("error update user", zap.Error(err))
+		return nil, errs.ERR_UPDATE_USER
+	}
+
+	return &response.UpdatedUserResponse{
+		IDEmployee: userExist.IDEmployee,
+		Username:   userExist.Username,
+		Name:       userExist.Name,
+		IsActive:   userExist.IsActive,
+		CreatedBy:  userExist.CreatedBy,
+		ModifiedBy: userExist.ModifiedBy,
+	}, nil
 }
 
-func (cu *userUsecase) FindUserById(ctx context.Context, id string) (entity.User, error) {
-	campaign, err := cu.repository.FindById(ctx, id)
+func (cu *userUsecase) DeleteByEmployeeIdUseCase(ctx context.Context, req_id request.GetIdUserRequest) (int, error) {
+
+	// _, err := cu.repository.FindByEmployeeId(ctx, req_id.IDEmployee)
+	// if err != nil {
+	// 	if errors.Is(err, gorm.ErrRecordNotFound) {
+	// 		utils.GetLogger().Error("user not found")
+	// 		return 0, errs.ERR_USER_NOT_FOUND
+	// 	}
+
+	// 	return 0, err
+	// }
+
+	err := cu.repository.DeleteByEmployeeIdRepo(ctx, req_id.IDEmployee)
 	if err != nil {
-		return campaign, err
+		utils.GetLogger().Error("error delete user", zap.Error(err))
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			utils.GetLogger().Error("user not found")
+			return 0, errs.ERR_USER_NOT_FOUND
+		}
+
+		return 0, errs.ERR_DELETE_USER
 	}
-	return campaign, nil
+
+	return 1, nil
+}
+
+// func (cu *userUsecase) FindAllUser(ctx context.Context) ([]entity.User, error) {
+// 	campaigns, err := cu.repository.FindAll(ctx)
+// 	if err != nil {
+// 		return campaigns, err
+// 	}
+
+// 	return campaigns, nil
+// }
+
+func (cu *userUsecase) FindUserByEmployeeId(ctx context.Context, id string) (*entity.User, error) {
+	userExist, err := cu.repository.FindByEmployeeId(ctx, id)
+	if err != nil {
+		return userExist, err
+	}
+	return userExist, err
 }
 
 func (usecase *userUsecase) SaveUser(ctx context.Context, req request.CreateUserRequest) (*response.CreatedUserResponse, error) {
