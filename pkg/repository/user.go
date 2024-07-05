ff6e55bfe09d13f7c48d11cf4e9d58778704986c
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-05 17:18:22 +0700

    update user.go

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"template-ulamm-backend-go/pkg/datasource"
 	"template-ulamm-backend-go/pkg/model/entity"
+
+	"gorm.io/gorm"
 )
 
 type UserRepository interface {
-	FindAll(ctx context.Context) ([]entity.User, error)
-	FindById(ctx context.Context, id string) (entity.User, error)
+	// FindAll(ctx context.Context) ([]entity.User, error)
+	FindByEmployeeId(context.Context, string) (*entity.User, error)
 	Save(context.Context, *entity.User) error
+	UpdateByEmployeeIdRepo(context.Context, *entity.User) error
+	DeleteByEmployeeIdRepo(context.Context, string) error
 }
 
 type userRepository struct {
@@ -23,27 +27,40 @@ func NewUserRepository(datasource *datasource.Datasource) UserRepository {
 }
 
 func (repo *userRepository) Save(ctx context.Context, data *entity.User) error {
-	return repo.Datasource.GormDB.WithContext(ctx).Omit("id").Create(&data).Error
+	return repo.Datasource.GormDB.WithContext(ctx).Create(&data).Error
 }
 
-func (cr *userRepository) FindAll(ctx context.Context) ([]entity.User, error) {
-	var users []entity.User
+func (repo *userRepository) UpdateByEmployeeIdRepo(ctx context.Context, data *entity.User) error {
+	return repo.Datasource.GormDB.WithContext(ctx).Updates(&data).Error
+}
 
-	err := cr.Datasource.GormDB.WithContext(ctx).Find(&users).Error
-	if err != nil {
-		return nil, err
+func (repo *userRepository) FindByEmployeeId(ctx context.Context, id string) (*entity.User, error) {
+	var result *entity.User
+
+	err := repo.Datasource.GormDB.WithContext(ctx).Where("id_employee = ?", id).First(&result).Error
+
+	return result, err
+}
+
+func (repo *userRepository) DeleteByEmployeeIdRepo(ctx context.Context, id string) error {
+	exec := repo.Datasource.GormDB.WithContext(ctx).Where("id_employee = ?", id).Delete(&entity.User{})
+	if err := exec.Error; err != nil {
+		return err
+	}
+	if exec.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
 	}
 
-	return users, nil
+	return nil
 }
 
-func (cr *userRepository) FindById(ctx context.Context, id string) (entity.User, error) {
-	var user entity.User = entity.User{IDEmployee: id}
+// func (cr *userRepository) FindAll(ctx context.Context) ([]entity.User, error) {
+// 	var users []entity.User
 
-	err := cr.Datasource.GormDB.WithContext(ctx).Where("id_employee = ?", id).First(&user).Error
-	if err != nil {
-		return user, err
-	}
+// 	err := cr.Datasource.GormDB.WithContext(ctx).Find(&users).Error
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return user, nil
-}
+// 	return users, nil
+// }
