deb5a754445c24f2a670e347c990a9994b86face
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-05 17:17:39 +0700

    update user.go

--- api/controller/user.go
+++ api/controller/user.go
@@ -12,9 +12,11 @@ import (
 )
 
 type UserController interface {
-	GetUser(ginCtx *gin.Context)
-	// GetOneUser(ginCtx *gin.Context)
+	// GetUser(ginCtx *gin.Context)
+	GetOneUser(ginCtx *gin.Context)
 	CreateUser(ginCtx *gin.Context)
+	UpdateUserByEmployeeIdController(ginCtx *gin.Context)
+	DeleteUserByEmployeeIdController(ginCtx *gin.Context)
 }
 
 type userController struct {
@@ -25,6 +27,58 @@ func NewUserController(userUsecase usecase.UserUsecase) UserController {
 	return &userController{
 		UserUsecase: userUsecase,
 	}
+}
+
+func (uc *userController) DeleteUserByEmployeeIdController(ginCtx *gin.Context) {
+	ctx, cancel := context.WithTimeout(ginCtx, TIMEOUT)
+	defer cancel()
+
+	var req_id request.GetIdUserRequest
+
+	err := ginCtx.ShouldBindUri(&req_id)
+	if err != nil {
+		utils.GetLogger().Error("failed validate request", zap.Error(err))
+		response.BadRequest(ginCtx, err)
+		return
+	}
+
+	resp, err := uc.UserUsecase.DeleteByEmployeeIdUseCase(ctx, req_id)
+	if err != nil {
+		response.FailedResponse(ginCtx, err)
+		return
+	}
+
+	response.SuccessResponse(ginCtx, "success delete user", resp)
+}
+
+func (uc *userController) UpdateUserByEmployeeIdController(ginCtx *gin.Context) {
+	ctx, cancel := context.WithTimeout(ginCtx, TIMEOUT)
+	defer cancel()
+
+	var req_id request.GetIdUserRequest
+
+	var req request.UpdateUserRequest
+
+	err := ginCtx.ShouldBindUri(&req_id)
+	if err != nil {
+		utils.GetLogger().Error("failed validate request", zap.Error(err))
+		response.BadRequest(ginCtx, err)
+		return
+	}
+
+	if err := request.ValidateRequest(ginCtx, &req); err != nil {
+		utils.GetLogger().Error("failed validate request", zap.Error(err))
+		response.BadRequest(ginCtx, err)
+		return
+	}
+
+	resp, err := uc.UserUsecase.UpdateByEmployeeIdUseCase(ctx, req, req_id)
+	if err != nil {
+		response.FailedResponse(ginCtx, err)
+		return
+	}
+
+	response.SuccessResponse(ginCtx, "success create user", resp)
 }
 
 func (uc *userController) CreateUser(ginCtx *gin.Context) {
@@ -49,27 +103,39 @@ func (uc *userController) CreateUser(ginCtx *gin.Context) {
 
 }
 
-func (uc *userController) GetUser(ginCtx *gin.Context) {
+// func (uc *userController) GetUser(ginCtx *gin.Context) {
 
-	ctx := ginCtx.Request.Context()
+// 	ctx := ginCtx.Request.Context()
 
-	users, err := uc.UserUsecase.FindAllUser(ctx)
-	if err != nil {
-		response.FailedResponse(ginCtx, err)
-		return
-	}
-
-	response.SuccessResponse(ginCtx, "success get user", users)
-}
-
-// func (uc *userController) GetOneUser(ginCtx *gin.Context) {
-
-// 	user, err := uc.UserUsecase.FindUserById(ginCtx.Param("id"))
-
+// 	users, err := uc.UserUsecase.FindAllUser(ctx)
 // 	if err != nil {
 // 		response.FailedResponse(ginCtx, err)
 // 		return
 // 	}
-// 	response.SuccessResponse(ginCtx, "success get user", user)
 
+// 	response.SuccessResponse(ginCtx, "success get user", users)
 // }
+
+func (uc *userController) GetOneUser(ginCtx *gin.Context) {
+	ctx, cancel := context.WithTimeout(ginCtx, TIMEOUT)
+	defer cancel()
+
+	userIdStr := ginCtx.Param("id")
+
+	if userIdStr == "" {
+		utils.GetLogger().Error("failed validate request")
+		response.BadRequest(ginCtx, nil)
+		return
+	}
+
+	resp, err := uc.UserUsecase.FindUserByEmployeeId(ctx, userIdStr)
+
+	if err != nil {
+		utils.GetLogger().Error("failed validate request", zap.Error(err))
+		response.FailedResponse(ginCtx, err)
+		return
+	}
+
+	response.SuccessResponse(ginCtx, "success get user", resp)
+
+}
