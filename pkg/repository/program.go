836d7a0161d264538928e601f9db7a73fd73d66e
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-08 15:17:31 +0700

    update program.go

--- pkg/repository/program.go
+++ pkg/repository/program.go
@@ -8,6 +8,7 @@ import (
 
 type ProgramRepository interface {
 	CreateProgramRepo(ctx context.Context, data *entity.Program) (*entity.Program, error)
+	GetAllProgramsRepo(ctx context.Context) ([]entity.Program, error)
 }
 
 type programRepository struct {
@@ -28,4 +29,13 @@ func (repo *programRepository) CreateProgramRepo(ctx context.Context, data *enti
 	}
 
 	return data, nil
+}
+
+func (repo *programRepository) GetAllProgramsRepo(ctx context.Context) ([]entity.Program, error) {
+	var programs []entity.Program
+	err := repo.Datasource.GormDB.WithContext(ctx).Order("id_program desc").Find(&programs).Error
+	if err != nil {
+		return programs, err
+	}
+	return programs, nil
 }
