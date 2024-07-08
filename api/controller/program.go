14c9d1e92dbc062ddb4abc1827b073681fd4be41
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-07-08 14:04:57 +0700

    feat: create program.go

---
+++ api/controller/program.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"context"
+	"template-ulamm-backend-go/pkg/model/request"
+	"template-ulamm-backend-go/pkg/model/response"
+	"template-ulamm-backend-go/pkg/usecase"
+	"template-ulamm-backend-go/utils"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type ProgramController interface {
+	CreateProgram(ginCtx *gin.Context)
+}
+
+type programController struct {
+	usecase.ProgramUsecase
+}
+
+func NewProgramController(programUsecase usecase.ProgramUsecase) ProgramController {
+	return &programController{
+		ProgramUsecase: programUsecase,
+	}
+}
+
+func (controller *programController) CreateProgram(ginCtx *gin.Context) {
+	ctx, cancel := context.WithTimeout(ginCtx, TIMEOUT)
+	defer cancel()
+
+	var req request.CreateProgramRequest
+
+	if err := request.ValidateRequest(ginCtx, &req); err != nil {
+		utils.GetLogger().Error("failed validate request", zap.Error(err))
+		response.BadRequest(ginCtx, err)
+		return
+	}
+
+	resp, err := controller.ProgramUsecase.CreateProgramUseCase(ctx, req)
+	if err != nil {
+		response.FailedResponse(ginCtx, err)
+		return
+	}
+	response.SuccessResponse(ginCtx, "success create program", resp)
+}
